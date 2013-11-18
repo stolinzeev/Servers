9331d163b622e63ccdbc1af39417b7649600ea81
Stolin Zeev <zeev@gettaxi.com>
2013-11-18 13:53:09 +0200

    auth

---
+++ GetTaxiServers/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GetTaxiServers::Application.config.secret_key_base = '6d8cdd998b4ad622dbc1adfc7caa4daa7ce00803feb4ec2a5ba4218ea6013b2ca81e7dc4047bd557e7595f431ef668d2a1fc48cbc40c1d7b9857dde815cbe3f3'
---
+++ GetTaxiServers/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GetTaxiServers::Application.config.session_store :cookie_store, key: '_GetTaxiServers_session'
